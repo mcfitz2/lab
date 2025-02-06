85ea30a8a62bd9f2f366ad27d83e81ba7b86cccf
Micah Fitzgerald <mcfitz2@gmail.com>
2025-02-06 16:27:00 -0600

    finish prototype and add dockerfile

--- applications/namada_exporter/src/main.rs
+++ applications/namada_exporter/src/main.rs
@@ -1,124 +1,131 @@
 use prometheus_exporter::{self, prometheus::register_gauge};
-use std::path::PathBuf;
 use std::str::FromStr;
-
-use clap::{Parser, Subcommand};
-use directories::ProjectDirs;
-use namada_sdk::chain::Epoch;
+use clap::{Parser};
 use namada_sdk::{
     address::Address,
-    chain::ChainId,
-    collections::HashSet,
-    error::{EncodingError, Error, Result},
-    io::{NamadaIo, StdIo},
-    key::common::CommonPublicKey,
-    masp::{fs::FsShieldedUtils, ShieldedContext},
     rpc,
-    rpc::TxBroadcastData,
-    signing::find_key_by_pk,
-    token,
-    tx::{self, ProcessTxResponse, Section, SignatureIndex, Tx},
-    wallet::fs::FsWalletUtils,
-    Namada, NamadaImpl,
 };
-use prometheus_exporter::prometheus::core::Number;
+use namada_sdk::uint::Uint;
+use prometheus_exporter::prometheus::core::{AtomicF64, GenericGauge};
 use tendermint_rpc::HttpClient;
 use tendermint_rpc::Url;
+
+const NAM_ADDRESS: &str = "tnam1q9gr66cvu4hrzm0sd5kmlnjje82gs3xlfg3v6nu7";
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    // #[arg(long)]
-    // pub validator_address: Address,
-    // #[arg(long)]
-    // pub account_address: Address,
-    #[arg(long, default_value = "namada.5f5de2dd1b88cba30586420")]
-    pub chain_id: String,
-    #[arg(long, default_value = base_dir())]
-    pub base_dir: std::path::PathBuf,
+    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
+    pub address: Vec<Address>,
     #[arg(long, default_value = "https://namada-rpc.mandragora.io")]
     pub rpc_url: String,
-    #[arg(long, default_value = "60")]
+    #[arg(long, default_value = "6")]
     pub frequency: u64,
-    // listen address
-    // port
-    // freqeuency
+    #[arg(long, default_value = "127.0.0.1")]
+    pub listen_address: String,
+    #[arg(long, default_value = "9186")]
+    pub listen_port: u32,
 }
-pub fn base_dir() -> String {
-    let proj_dirs = ProjectDirs::from("", "", "Namada").expect("Unable to get base dir from OS");
-    proj_dirs.clone().data_dir().display().to_string()
-}
-type NamadaCtx = NamadaImpl<HttpClient, FsWalletUtils, FsShieldedUtils, StdIo>;
 
-pub async fn create_client(rpc_url: &str, base_dir: PathBuf, chain_id: &str) -> HttpClient {
+pub async fn create_client(rpc_url: &str) -> HttpClient {
     let url = Url::from_str(rpc_url).expect("Invalid RPC address");
     HttpClient::new(url).unwrap()
-
-    // let wallet_dir = base_dir.clone().join(chain_id);
-    // let wallet = FsWalletUtils::new(wallet_dir.clone());
-    // let masp_dir = base_dir.join("./masp");
-
-    // let shielded_ctx = ShieldedContext::new(FsShieldedUtils::new(masp_dir));
-    // let std_io = StdIo;
-
-    // NamadaImpl::new(http_client, wallet, shielded_ctx.into(), std_io)
-    //     .await
-    //     .expect("unable to initialize Namada context")
 }
 
-async fn fetch_epoch(client: &HttpClient) -> f64 {
-    match rpc::query_epoch(client).await {
-        Ok(current_epoch) => current_epoch.0.into_f64(),
-        Err(e) => -1.0,
-    }
-}
 
 async fn fetch_rewards(client: &HttpClient, address: Address) -> f64 {
     match rpc::query_rewards(client, &None, &address, &None).await {
         Ok(rewards) => f64::from(rewards.raw_amount().as_u32()) / 1000000.0,
-        Err(e) => -1.0,
+        Err(_e) => -1.0,
     }
 }
 
 async fn fetch_balance(client: &HttpClient, address: Address) -> f64 {
-    match rpc::get_token_balance(client, &Address::decode("tnam1q9gr66cvu4hrzm0sd5kmlnjje82gs3xlfg3v6nu7").unwrap(), &address, None).await {
-        Ok(rewards) => f64::from(rewards.raw_amount().as_u32()) / 1000000.0,
-        Err(e) => -1.0,
+    let nam = Address::decode(NAM_ADDRESS).unwrap();
+
+    match rpc::get_token_balance(client, &nam, &address, None).await {
+        Ok(balance) => {
+            f64::from(balance.raw_amount().as_u32()) / 1000000.0
+        },
+        Err(e) => {
+            eprintln!("{}", e);
+            -1.0
+        },
     }
 }
 
-async fn fetch_balance(client: &HttpClient, address: Address) -> f64 {
-    match rpc::get_token_balance(client, &Address::decode("tnam1q9gr66cvu4hrzm0sd5kmlnjje82gs3xlfg3v6nu7").unwrap(), &address, None).await {
-        Ok(rewards) => f64::from(rewards.raw_amount().as_u32()) / 1000000.0,
-        Err(e) => -1.0,
+async fn fetch_staked(client: &HttpClient, address: Address) -> f64 {
+    match rpc::query_epoch(client).await {
+         Ok(epoch) => {
+             match rpc::get_validator_stake(client, epoch, &address).await {
+                 Ok(stake) => (stake.raw_amount().as_u64() / 1000000) as f64,
+                 Err(_e) => 0.0,
+             }
+         }
+         Err(_e) => 0.0
     }
 }
 
+async fn fetch_voting_power(client: &HttpClient, address: Address) -> f64 {
+    match rpc::query_epoch(client).await {
+        Ok(epoch) => {
+            match rpc::get_validator_stake(client, epoch, &address).await {
+                Ok(stake) => {
+                    let staked = stake.raw_amount().checked_div(Uint::from(1000000)).unwrap().as_u32();
+                    match rpc::get_total_active_voting_power(client, epoch).await {
+                        Ok(total_voting_power) => {
+                            f64::from(staked) / f64::from(total_voting_power.raw_amount().checked_div(Uint::from(1000000)).unwrap().as_u32())
+                        },
+                        Err(_e) => -1.0,
+                    }
+                },
+                Err(_e) => -1.0,
+            }
+        }
+        Err(_e) => -1.0
+    }
+}
+
+
+struct AddressGaugeSet {
+    rewards: GenericGauge<AtomicF64>,
+    balance: GenericGauge<AtomicF64>,
+    voting_power: GenericGauge<AtomicF64>,
+    staked: GenericGauge<AtomicF64>,
+    address: Address,
+}
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
+    let client = create_client(&args.rpc_url).await;
+    
+    let gauges = args.address.iter().map(
+        |x| AddressGaugeSet {
+            rewards: register_gauge!(format!("namada_{}_rewards", x), "claimable rewards").unwrap(),
+            balance: register_gauge!(format!("namada_{}_balance", x), "current account balance in NAM").unwrap(),
+            voting_power: register_gauge!(format!("namada_{}_voting_power", x), "current voting power").unwrap(),
+            staked: register_gauge!(format!("namada_{}_staked", x), "NAM staked to this validator").unwrap(),
+            address: x.clone()
+        }
+    ).collect::<Vec<AddressGaugeSet>>();
 
-    // let binding = "0.0.0.0:9186".parse().unwrap();
-    // let exporter = prometheus_exporter::start(binding).unwrap();
+    let binding = "0.0.0.0:9186".parse().unwrap();
+    let exporter = prometheus_exporter::start(binding).unwrap();
 
-    // let rewards = register_gauge!("namada_rewards", "claimable rewards").unwrap();
-    // let balance = register_gauge!("namada_balance", "current account balance in NAM").unwrap();
-    // let voting_power = register_gauge!("namada_voting_power", "current voting power").unwrap();
-    // let staked = register_gauge!("namada_staked", "NAM staked to this validator").unwrap();
-    // let duration = std::time::Duration::from_millis(args.frequency * 1000);
 
-    // loop {
-    let ctx = create_client(&args.rpc_url, args.base_dir.clone(), &args.chain_id).await;
+    let duration = std::time::Duration::from_millis(args.frequency * 1000);
 
-    //     let guard = exporter.wait_duration(duration);
-    //     println!("Fetching rewards");
-    //     rewards.set(fetch_rewards(ctx, Address::decode("tnam1q9252ynj250dw9d0n96tf9yaycljmpatfvgrj0th").unwrap()).await);
-    //     drop(guard);
-    // }
-    let validators = vec![Address::decode("tnam1q9252ynj250dw9d0n96tf9yaycljmpatfvgrj0th").unwrap(), Address::decode("tnam1q9cysu5x8rphc9w92wglzys6kd5kqt3mdvp9sxz8").unwrap()];
-    for v in validators {
-        println!("Rewards available {}", fetch_rewards(&ctx, v.clone()).await);
-        println!("Balance available {}", fetch_balance(&ctx, v.clone()).await);
+    loop {
+
+        let guard = exporter.wait_duration(duration);
+        println!("Fetching metrics");
+        for set in gauges.iter() {
+            set.rewards.set(fetch_rewards(&client, set.address.clone()).await);
+            set.balance.set(fetch_balance(&client, set.address.clone()).await);
+            set.voting_power.set(fetch_voting_power(&client, set.address.clone()).await);
+            set.staked.set(fetch_staked(&client, set.address.clone()).await);
+        }
+        drop(guard);
+        println!("Fetching metrics complete");
     }
-    println!("Current epoch {}", fetch_epoch(&ctx).await);
 }
